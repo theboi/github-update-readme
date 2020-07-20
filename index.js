59673884d74816005897ad4e19627a8a78cd40b0
Ryan The <ryan.the.2006@gmail.com>
2020-07-20 18:38:16 +0800

    Add async

--- index.js
+++ index.js
@@ -40,17 +40,18 @@ ${core.getInput('subtitle')}
 ||||
 | :-: | :-: | :-: |
 ${chunkArray(Array.from(recentRepos), 3).map((value) => {
-  return `| ${value.map(value => ` **[${value}](https://github.com/${value})** |`)}
-  | ${value.map(async value => {
-    const image = await octokit.request('/repos/{owner}/{repo}/contents/{path}', {
-      owner: username,
-      repo: repo,
-      path: "DISPLAY.jpg"
-    })
-    console.log(image)
-    return ` <a href="https://github.com/${value}"><img src="https://github.com/${value}/raw/master/DISPLAY.jpg" alt="${value}" title="Cover Image" width="150" height="150"></a> |`
-  })}\n`
-}).toString().replace(/,/g, "")}
+      return `| ${value.map(value => ` **[${value}](https://github.com/${value})** |`)}
+  | ${value.map(async (value) => {
+        const image = await octokit.request('/repos/{owner}/{repo}/contents/{path}', {
+          owner: username,
+          repo: repo,
+          path: "DISPLAY.jpg"
+        })
+        console.log(image)
+
+        return ` <a href="https://github.com/${value}"><img src="https://github.com/${value}/raw/master/DISPLAY.jpg" alt="${value}" title="Cover Image" width="150" height="150"></a> |`
+      })}\n`
+    }).toString().replace(/,/g, "")}
 
 ---
 
@@ -58,8 +59,8 @@ ${chunkArray(Array.from(recentRepos), 3).map((value) => {
 
 **${core.getInput('footer')}**
 `
-    console.log("chunky", )
-    
+    console.log("chunky",)
+
     const putReadme = await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
       owner: username,
       repo: repo,
