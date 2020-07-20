3670636312a85a9ba22224868c24fab17eedc316
Ryan The <ryan.the.2006@gmail.com>
2020-07-20 13:05:52 +0800

    Fix sha

--- index.js
+++ index.js
@@ -3,39 +3,41 @@ const github = require("@actions/github");
 const { Octokit } = require('@octokit/core')
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
-try {
-  const data = `
-## ${core.getInput('title')}
+(async () => {
+  try {
+    const data = `
+  ## ${core.getInput('title')}
+    
+  ### ${core.getInput('subtitle')}
+  `
   
-### ${core.getInput('subtitle')}
-`
+    const username = process.env.GITHUB_REPOSITORY.split("/")[0]
+    const repo = process.env.GITHUB_REPOSITORY.split("/")[1]
+    const getReadme = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+      owner: username,
+      repo: repo,
+      path: core.getInput('path'),
+    })
+    const sha = getReadme.data.sha
 
-  const username = process.env.GITHUB_REPOSITORY.split("/")[0]
-  const repo = process.env.GITHUB_REPOSITORY.split("/")[1]
-  const sha = process.env.GITHUB_SHA
+    console.log("Sha ", sha)
+  
+    const putReadme = await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
+      owner: username,
+      repo: repo,
+      path: core.getInput('path'),
+      message: '(Automated) Update README.md',
+      content: "Hi",
+      sha: sha,
+      committer: {
+        name: "Ryan The",
+        email: "ryan.the.2006@gmail.com"
+      }
+    })
+    console.log("RESPONSE: ", putReadme)
 
-  console.log(data)
-  console.log("Username2 ", username)
-  console.log("Sha ", sha)
-
-  octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
-    owner: username,
-    repo: repo,
-    path: core.getInput('path'),
-    message: '(Automated) Update README.md',
-    content: "Hi",
-    sha: sha,
-    committer: {
-      name: "Ryan The",
-      email: "ryan.the.2006@gmail.com"
-    }
-  }).then(response => {
-    console.log("RESPONSE: ", response)
-  }).catch(e => {
-    console.error("ERROR: ", e)
-  })
-
-} catch (e) {
-  console.error(e)
-  core.setFailed(e.message)
-}
+  } catch (e) {
+    console.error(e)
+    core.setFailed(e.message)
+  }
+})()
