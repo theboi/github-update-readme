363a25a7614577a7f64ede0b59fc45400e8b016a
Ryan The <ryan.the.2006@gmail.com>
2020-07-20 18:27:25 +0800

    Add debug

--- index.js
+++ index.js
@@ -41,7 +41,15 @@ ${core.getInput('subtitle')}
 | :-: | :-: | :-: |
 ${chunkArray(Array.from(recentRepos), 3).map((value) => {
   return `| ${value.map(value => ` **[${value}](https://github.com/${value})** |`)}
-  | ${value.map(value => ` <a href="https://github.com/${value}"><img src="https://github.com/${value}/raw/master/COVER.jpg" alt="${value} Cover Image" width="150" height="150"></a> |`)}\n`
+  | ${value.map(value => {
+    const image = await octokit.request('/repos/{owner}/{repo}/contents/{path}', {
+      owner: username,
+      repo: repo,
+      path: "DISPLAY.jpg"
+    })
+    console.log(image)
+    return ` <a href="https://github.com/${value}"><img src="https://github.com/${value}/raw/master/COVER.jpg" alt="${value}" title="Cover Image" width="150" height="150"></a> |`
+  })}\n`
 }).toString().replace(/,/g, "")}
 
 ---
