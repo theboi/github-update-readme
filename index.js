6658500a2ce37266b5c824fac2decf07bd68cd7d
Ryan The <ryan.the.2006@gmail.com>
2020-07-20 18:58:19 +0800

    Fix async

--- index.js
+++ index.js
@@ -39,18 +39,19 @@ ${core.getInput('subtitle')}
 
 ||||
 | :-: | :-: | :-: |
-${chunkArray(Array.from(recentRepos), 3).map(async (value) => {
-      return `| ${value.map(async value => ` **[${value}](https://github.com/${value})** |`)}
-  | ${value.map(async (value) => {
-        // let imageSource = value
-        // await octokit.request('/repos/{owner}/{repo}/contents/{path}', {
-        //   owner: value.split("/")[0],
-        //   repo: value.split("/")[1],
-        //   path: "DISPLAY.jpg"
-        // }).catch((e) => {
-        //   if (e.status === "404") imageSource = `${username}/${repo}`
-        // })
-        return ` <a href="https://github.com/${value}"><img src="https://github.com/${value}/raw/master/DISPLAY.jpg" alt="${value}" title="Cover Image" width="150" height="150"></a> |`
+${chunkArray(Array.from(recentRepos), 3).map((value) => {
+      return `| ${value.map(value => ` **[${value}](https://github.com/${value})** |`)}
+  | ${value.map((value) => {
+        let imageSource = value
+        octokit.request('/repos/{owner}/{repo}/contents/{path}', {
+          owner: value.split("/")[0],
+          repo: value.split("/")[1],
+          path: "DISPLAY.jpg"
+        }).catch((e) => {
+          if (e.status === "404") imageSource = `${username}/${repo}`
+        }).then(() => {
+          return ` <a href="https://github.com/${value}"><img src="https://github.com/${value}/raw/master/DISPLAY.jpg" alt="${value}" title="Cover Image" width="150" height="150"></a> |`
+        })
       })}\n`
     }).toString().replace(/,/g, "")}
 
